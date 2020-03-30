b9c7a4ac5562de9d519f76ca0d1c4fd1b99f5941
ykcaj-i <jliuarc@gmail.com>
2020-03-30 02:29:32 -0400

    update js

--- bin/bin.js
+++ bin/bin.js
@@ -1,1 +1,4 @@
 // <script type = "text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>//
+
+//<script type = "text/javascript" src = "jquery-3.4.1.js"></script>
+//<script src = "js/main.js"></script>
