dfc4f5e96cb035ec63c84a42ffe5e9c3cba59b01
ykcaj-i <jliuarc@gmail.com>
2020-03-30 01:52:46 -0400

    changes

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var topnav = document.getElementById("topnav");
 // get the offset position of the navbar
 var sticky = topnav.offsetTop;
 
-// add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// add the sticky class to the navbar when you reach its scroll position. r
+// remove "sticky" when you leave the scroll position
 function goScroll() {
   if (window.pageYOffset >= sticky) {
     topnav.classList.add("sticky")
