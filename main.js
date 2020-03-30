0328d799f9a466902539d5494e8511ffbaa229f8
ykcaj-i <jliuarc@gmail.com>
2020-03-30 01:51:29 -0400

    chnages

---
+++ main.js
@@ -0,0 +1,17 @@
+// execute myFunction when user scroll
+window.onscroll = function() {goScroll()};
+
+// navbar
+var topnav = document.getElementById("topnav");
+
+// get the offset position of the navbar
+var sticky = topnav.offsetTop;
+
+// add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function goScroll() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
