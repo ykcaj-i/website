dcde47fcfb95f0e25763d206358b6dbedf20b08f
ykcaj-i <jliuarc@gmail.com>
2020-03-30 05:13:08 -0400

    updates

--- bin/bin.js
+++ bin/bin.js
@@ -2,3 +2,39 @@
 
 //<script type = "text/javascript" src = "jquery-3.4.1.js"></script>
 //<script src = "js/main.js"></script>
+
+/*
+// execute myFunction when user scroll
+
+window.onscroll = function() {myFunction()};
+
+// navbar
+var topnav = document.getElementById("topnav");
+
+// get the offset position of the navbar
+var sticky = topnav.offsetTop;
+
+// add the sticky class to the navbar when you reach its scroll position. r
+// remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
+
+
+
+<script type = "text/javascript">
+
+
+$(window).on("scroll", function(){
+  if($window).scrollTop()){
+    $('topnav').addClass('topnav')
+  }
+  else{
+    $('topnav').removeClass('topnav')
+  }
+})
+*/
